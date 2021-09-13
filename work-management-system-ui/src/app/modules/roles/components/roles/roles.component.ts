5219301e5419c464e93fdfac5436d034667342a6
Karol Krzywoń <karokrz849@student.polsl.pl>
2021-09-14 01:14:45 +0200

    merge

--- work-management-system-ui/src/app/modules/roles/components/roles/roles.component.ts
+++ work-management-system-ui/src/app/modules/roles/components/roles/roles.component.ts
@@ -11,19 +11,7 @@ import { RoleService } from 'src/app/models/roles/role.service';
 export class RolesComponent implements OnInit {
   accessLevels: Array<number> = [0, 1, 2, 3];
   title: string = 'Role:';
-<<<<<<< Updated upstream
-  data: Array<any>;
-  constructor(){
-    this.data = [
-        { role: 'Rola1', value1: Boolean,value2: Boolean },
-        { role: 'Rola2', value1: Boolean,value2: Boolean },
-        { role: 'Rola3', value1: Boolean,value2: Boolean },
-        { role: 'Rola4', value1: Boolean,value2: Boolean }
-    ];
-}
-=======
   roles: Role[];
->>>>>>> Stashed changes
 
   constructor(
     private roleService: RoleService
