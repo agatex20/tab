3a6e8077962b3b34f550118666014a79b6021ab9
agatex20 <nie.posiadam.emaila@gmail.com>
2021-09-14 00:20:59 +0200

    request w report

--- work-management-system-ui/src/app/modules/report/components/report/report.component.ts
+++ work-management-system-ui/src/app/modules/report/components/report/report.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
 import { Absence } from 'src/app/models/absence/absence.model';
+import { AbsenceService } from 'src/app/models/absence/absence.service';
+import { AbsenceType } from 'src/app/models/absenceType/absence-type.model';
+import { AbsenceTypeService } from 'src/app/models/absenceType/absence-type.service';
 import { LeaveRequest } from 'src/app/models/leave-requests/leave-request.model';
 import { User } from 'src/app/models/user/user.model';
+import { UserService } from 'src/app/models/user/user.service';
 
 @Component({
   selector: 'app-report',
@@ -10,13 +15,57 @@ import { User } from 'src/app/models/user/user.model';
 })
 export class ReportComponent implements OnInit {
   title: string = 'Raport';
+  leftDays: string = '0'
   startDate: string;
   endDate: string;
   employees: User[];
   absences: Absence[]; 
-  constructor() { }
-
+  absenceTypes: AbsenceType[];
+  
+  constructor(
+    private userService: UserService,
+    private absenceService: AbsenceService,
+    private absenceTypeService: AbsenceTypeService
+  ){
+    this.loadEmployees();
+    this.loadAbsenceTypes();
+}
   ngOnInit(): void {
+  }
+
+  loadEmployees() {
+    this.userService.getAll()
+      .pipe(first())
+      .subscribe(employees => this.employees = employees);
+  }
+
+  loadAbsences(userId: string) {
+    this.absenceService.getAbsence(userId)
+      .pipe(first())
+      .subscribe(absences => this.absences = absences);
+  }
+
+  loadAbsenceTypes() {
+    this.absenceTypeService.getAll()
+      .pipe(first())
+      .subscribe(absenceTypes => this.absenceTypes = absenceTypes);
+  }
+
+  selectChangeHandler(event: any) {
+    console.log(event.target.value);
+    this.loadAbsences(event.target.value);
+  }
+
+  getTypeName(id: string) {
+    const type =  this.absenceTypes.find((type) => type.absenceTypeId === id)
+    if (!type) {
+      return ''
+    }
+    return type.name
+  }
+
+  getDate(date: string) {
+    return date.substring(0,10);
   }
 
   onSubmit(){
--- work-management-system-ui/src/app/modules/roles/components/roles/roles.component.ts
+++ work-management-system-ui/src/app/modules/roles/components/roles/roles.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class RolesComponent implements OnInit {
 
   title: string = 'Role:';
-  data: Array<any>;constructor(){
+  data: Array<any>;
+  constructor(){
     this.data = [
         { role: 'Rola1', value1: Boolean,value2: Boolean },
         { role: 'Rola2', value1: Boolean,value2: Boolean },
