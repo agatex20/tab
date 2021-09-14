9e5268d6e16fa53432d5943dee9ae2885f86acc4
a3ondel <a3ondel@gmail.com>
2021-09-14 18:40:19 +0200

    added consts files and post absences

--- work-management-system-ui/src/app/modules/login/components/login-page/login-data.component.ts
+++ work-management-system-ui/src/app/modules/login/components/login-page/login-data.component.ts
@@ -1,50 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-
+import { AbsencesService } from 'src/app/services/absences.service';
 
 @Component({
   selector: 'app-login-data',
   templateUrl: './login-data.component.html',
-  styleUrls: ['./login-data.component.css']
+  styleUrls: ['./login-data.component.css'],
 })
 export class LoginDataComponent implements OnInit {
-login: string = '';
-password: string = '';
-title: string = 'Zaloguj';
+  login: string = '';
+  password: string = '';
+  title: string = 'Zaloguj';
 
+  constructor(private router: Router, private abs: AbsencesService) {}
 
-
-  constructor(private router: Router,
-              ) {
-
-
-  }
-
-  ngOnInit(): void {
-  }
-  onSubmit(){
-
-    if(!this.login)
-    {
+  ngOnInit(): void {}
+  onSubmit() {
+    if (!this.login) {
       alert('Wpisz login');
       return;
     }
 
-    if(!this.password)
-    {
+    if (!this.password) {
       alert('Wpisz haslo');
       return;
     }
     const newLoginData = {
       login: this.login,
-      password: this.password
+      password: this.password,
     };
 
     this.router.navigate(['main']);
     this.login = '';
     this.password = '';
-
-
   }
-
 }
--- work-management-system-ui/src/app/services/absences.service.ts
+++ work-management-system-ui/src/app/services/absences.service.ts
@@ -1,9 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UrlConsts } from '../constants';
+import { AbsenceDTO } from '../dto/absenceDTO';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AbsencesService {
   constructor(private http: HttpClient) {}
+
+  eg: AbsenceDTO = {
+    absenceTypeId: '5fb0a622-8058-440c-a3c6-385544dafae0',
+    startDate: new Date(),
+    endDate: new Date(),
+    userId: '160E4F02-24C3-4660-23CD-08D93B33A952',
+  };
+  send(absence: AbsenceDTO): Observable<AbsenceDTO> {
+    return this.http.post<AbsenceDTO>(
+      UrlConsts.localhost + '/Absences',
+      absence
+    );
+  }
 }
