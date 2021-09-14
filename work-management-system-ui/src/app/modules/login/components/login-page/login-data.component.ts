290850a0737ba690918ae2b60b01319aafaaef38
a3ondel <a3ondel@gmail.com>
2021-09-14 21:36:31 +0200

    roles services, enum fix

--- work-management-system-ui/src/app/modules/login/components/login-page/login-data.component.ts
+++ work-management-system-ui/src/app/modules/login/components/login-page/login-data.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { AccessLevelEnum } from 'src/app/dto/accessLevelEnum';
+
 import { AbsencesService } from 'src/app/services/absences.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { RolesService } from 'src/app/services/roles.service';
 
 @Component({
   selector: 'app-login-data',
@@ -13,18 +16,13 @@ export class LoginDataComponent implements OnInit {
   password: string = '';
   title: string = 'Zaloguj';
 
-  constructor(private router: Router, private abs: AuthService) {}
+  constructor(private router: Router, private abs: RolesService) {}
 
   ngOnInit(): void {
-    this.abs
-      .register({
-        email: 'jpestka',
-        password: 'string',
-        firstName: 'Jakub',
-        lastName: 'Pestka',
-      }) // .getFromWorkerConfirmed('260E4F02-24C3-4660-23CD-08D93B33A952')
-      //   .getWorkersAbsences('260E4F02-24C3-4660-23CD-08D93B33A952')
-      .subscribe({ next: (x) => console.log(x) });
+    this.abs.getAll().subscribe({ next: (x) => console.log(x) });
+    // .getFromWorkerConfirmed('260E4F02-24C3-4660-23CD-08D93B33A952')
+    //   .getWorkersAbsences('260E4F02-24C3-4660-23CD-08D93B33A952')
+    // .subscribe({ next: (x) => console.log(x) });
   }
   onSubmit() {
     if (!this.login) {
