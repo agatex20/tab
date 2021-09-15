68645e5d38668de317488d8c64542ffcc3ea1ba9
a3ondel <a3ondel@gmail.com>
2021-09-15 15:06:29 +0200

    deprecated marker on authenticationservice

--- work-management-system-ui/src/app/authentication/authentication.service.ts
+++ work-management-system-ui/src/app/authentication/authentication.service.ts
@@ -4,19 +4,24 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { User } from '../models/user/user.model';
-import { Configuration } from "../config";
+import { Configuration } from '../config';
+import { deprecated } from 'deprecated-decorator';
 
+@deprecated('use AuthService instead')
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
-
   public currentUser: Observable<User>;
-  private _isLoggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private _isLoggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    false
+  );
   private currentUserSubject: BehaviorSubject<User>;
 
   constructor(private http: HttpClient) {
-    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUserSubject = new BehaviorSubject<User>(
+      JSON.parse(localStorage.getItem('currentUser'))
+    );
     this.currentUser = this.currentUserSubject.asObservable();
   }
 
@@ -29,14 +34,20 @@ export class AuthenticationService {
   }
 
   login(username: string, password: string) {
-    return this.http.post<any>(`${Configuration.API_URL}authorization/login`, {username, password})
-      .pipe(map(user => {
-        // store user details and jwt token in local storage to keep user logged in between page refreshes
-        localStorage.setItem('currentUser', JSON.stringify(user));
-        this.currentUserSubject.next(user);
-        this._isLoggedIn.next(true);
-        return user;
-      }));
+    return this.http
+      .post<any>(`${Configuration.API_URL}authorization/login`, {
+        username,
+        password,
+      })
+      .pipe(
+        map((user) => {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+          this._isLoggedIn.next(true);
+          return user;
+        })
+      );
   }
 
   logout() {
